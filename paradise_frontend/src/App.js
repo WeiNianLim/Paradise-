6e315472a84316cd8b46cf2ad909d4a005049c4c
Victor Cho <V.Cho@hotmail.ca>
2018-08-02 14:47:56 -0700

    navbar creation

--- paradise_frontend/src/App.js
+++ paradise_frontend/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import {Switch, Route, Link} from 'react-router-dom'
 import Home from './Components/Home'
 import Form from './Components/FilterForm'
+import Header from './Components/Header'
 import './Components/App.css'
 
 
@@ -9,7 +10,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-      
+            <Header />
             <Home />
             <Form />
 
---
+++ paradise_frontend/src/Components/Header.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+          <nav class="navbar navbar-expand-lg navbar-light bg-light">
+              <a class="navbar-brand" href="#">Logo</a>
+              <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor03" aria-controls="navbarColor03" aria-expanded="false" aria-label="Toggle navigation" wfd-id="588">
+                  <span class="navbar-toggler-icon" wfd-id="439"></span>
+              </button>
+
+              <div class="collapse navbar-collapse" id="navbarColor03" wfd-id="431">
+                  <ul class="navbar-nav mr-auto" wfd-id="433">
+                     
+                  </ul>
+                  <form class="form-inline my-2 my-lg-0" wfd-id="432">
+                          <button class="btn btn-secondary my-2 my-sm-0" type="submit" wfd-id="587">Login</button>
+    </form>
+  </div>
+</nav>
+    </div>
+  );
+};
+
+export default Header;
