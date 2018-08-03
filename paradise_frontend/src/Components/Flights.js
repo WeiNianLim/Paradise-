1cfca578eca8c9c0b2cf86d2c67e15d39b33591d
Kaeleb Drew <kaelebdrew@gmail.com>
2018-08-02 22:20:18 -0700

    added more style to flights

--- paradise_frontend/src/Components/Flights.js
+++ paradise_frontend/src/Components/Flights.js
@@ -9,12 +9,12 @@ class Flights extends Component {
             <div className="media flightCard">
                 <img className="mr-3 flightsImage" src="http://104.236.16.159/wp-content/uploads/2013/03/Screen-shot-2013-03-17-at-11.27.43-PM.png" alt="Generic placeholder image"/>
                 <div className="media-body text-left">
-                    <h5 className="mt-0">{flight.brand} Flight No: {flight.flightNumber}</h5>
+                    <h5 className="mt-0 flightInfo">{flight.brand} Flight No: {flight.flightNumber}</h5>
                     <span>Departure:   {flight.departureTime}</span><br/>
                     <span>Arrival:  {flight.arrivalTime}</span>
                 </div>
                 <div className='mediaBody'>
-                <button className='btn btn-md btn-primary'>Select flight</button>
+                <button className='btn btn-md btn-outline-secondary'>Select flight</button>
                 </div>
             </div>
         )
