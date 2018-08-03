dac4f8c362c6399389bd73ad552afed9ba5a2e94
Victor Cho <V.Cho@hotmail.ca>
2018-08-03 10:01:31 -0700

    Style for itinerary

--- paradise_frontend/src/Components/Activity.js
+++ paradise_frontend/src/Components/Activity.js
@@ -6,6 +6,8 @@ const Activity = (props) => {
         <h3>{props.name}</h3>
         <h5>Location: {props.location}</h5>
         <h6>Contact: {props.contact}</h6>
+        <button>Delete</button>
+        <button>Refresh</button>
         </div>
      );
 }
