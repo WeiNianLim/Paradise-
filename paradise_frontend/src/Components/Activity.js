c5a208aa86874b6a68df52b4267bb86af6f48dc9
Victor Cho <V.Cho@hotmail.ca>
2018-08-02 23:25:35 -0700

    completed itinerary

--- paradise_frontend/src/Components/Activity.js
+++ paradise_frontend/src/Components/Activity.js
@@ -3,7 +3,9 @@ import React from 'react';
 const Activity = (props) => {
     return ( 
         <div>
-        <h1>{props.name[0].name}</h1>
+        <h3>{props.name}</h3>
+        <h5>Location: {props.location}</h5>
+        <h6>Contact: {props.contact}</h6>
         </div>
      );
 }
--- paradise_frontend/src/Components/Itinerary.js
+++ paradise_frontend/src/Components/Itinerary.js
@@ -1,23 +1,39 @@
-import React, { Component } from 'react';
-import Activity from "./Activity"
+import React, { Component } from "react";
+import Activity from "./Activity";
+import styles from "./Itinerary.css"
 
 class Itinerary extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {  }
-    }
-    render() { 
-        return ( 
-            <div>
-            <h1>August 10,2018</h1>
-            <Activity 
-            name={this.props.restaurants}
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
 
+  render() {
+    return (
+      <div>
+        <h1>August 10,2018</h1>
+        
+        {this.props.restaurants.map((item, index) => {
+          console.log(item.name);
+          return (
+              <div className={styles.itineraryItem}>
+            <Activity
+              name={item.name}
+              contact={item.display_phone}
+              location={
+                item.location.address1 + 
+                " " + 
+                item.location.display_address[1]
+              }
             />
-            {console.log(this.props.restaurants[0].name)}
+            <button>Delete</button>
+            <button>Refresh</button>
             </div>
-         );
-    }
+          );
+        })}
+      </div>
+    );
+  }
 }
- 
+
 export default Itinerary;
